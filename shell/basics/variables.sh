c03eda69a75b1914b1f668c72f7779d996aeb435
Vishnu Rao K <vishnu.rao.k1@gmail.com>
2025-04-18 21:37:41 +0200

    Created a new bash shell script showing variables assignment and printing them.

---
+++ shell/basics/variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assigning a value to a variable
+greeting="Hello, User!"
+
+# Printing the variable
+echo $greeting
+
+# Assiging values to multiple variables
+movie_name="The Dark Knight"
+movie_year=2008
+movie_rating=9.0
+movie_director="Christopher Nolan"
+
+# Printing the variables
+echo "Movie Name: $movie_name"
+echo "Movie Year: $movie_year"
+echo "Movie Rating: $movie_rating"
+echo "Movie Director: $movie_director"
