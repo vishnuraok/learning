890d9ad8a550237c5da5e8a88338ebd85f609a38
Vishnu Rao K <vishnu.rao.k1@gmail.com>
2026-03-15 13:50:39 +0100

    Add configuration files for Bash aliases, Git aliases, and Vim settings

---
+++ shell/config/bash_aliases.sh
@@ -0,0 +1,7 @@
+# Alias definitions.
+alias l="ls -lrt"
+alias ll="ls -lrta"
+alias ld="ls -ld"
+alias ..="cd .."
+alias ...="cd ../.."
+alias cd..="cd .."
---
+++ shell/config/git_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Git configuration
+git config --global color.ui auto
+
+# Set up some useful aliases
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.pl pull
+git config --global alias.pr "pull --rebase"
+git config --global alias.ft "fetch --prune"
+git config --global alias.df diff
+git config --global alias.lg "log --oneline --graph --all"
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.last "log -1 HEAD"
